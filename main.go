475d9e6da87b812174ce9f5ee8c9e3070b92d73f
pavel.yeliseev <jelisejev@@users.noreply.github.com>
2019-03-11 18:35:14 +0200

    parse csv file

--- main.go
+++ main.go
@@ -1,9 +1,37 @@
 package main
 
 import (
+	"bufio"
+	"encoding/csv"
 	"fmt"
+	"io"
+	"log"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hey!")
+	args := os.Args[1:]
+
+	if len(args) == 0 {
+		log.Fatalln("No input file provided")
+	}
+
+	input := args[0]
+
+	fmt.Println(input)
+
+	file, _ := os.Open(input)
+	reader := csv.NewReader(bufio.NewReader(file))
+	reader.Comma = ';'
+
+	for {
+		line, error := reader.Read()
+
+		if error == io.EOF {
+			break
+		} else if error != nil {
+			log.Fatal(error)
+		}
+	}
+
 }
