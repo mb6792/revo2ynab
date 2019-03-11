a9adc848900e0b888410cdadc6d67e6509900ff8
pavel.yeliseev <jelisejev@@users.noreply.github.com>
2019-03-11 19:02:27 +0200

    write csv file

--- main.go
+++ main.go
@@ -28,11 +28,14 @@ func main() {
 	fmt.Println(input)
 
 	file, _ := os.Open(input)
+	defer file.Close()
+
 	reader := csv.NewReader(bufio.NewReader(file))
 	reader.LazyQuotes = true
 	reader.Comma = ';'
 
 	var entries []RevoEntry
+	reader.Read()
 	for {
 		line, error := reader.Read()
 
@@ -51,4 +54,20 @@ func main() {
 	}
 	fmt.Println(entries)
 
+	target, _ := os.Create("ynab.csv")
+	defer target.Close()
+
+	writer := csv.NewWriter(target)
+	defer writer.Flush()
+	for _, record := range entries {
+		error := writer.Write([]string{
+			record.Date,
+			record.Reference,
+			record.PaidOut,
+		})
+
+		if error != nil {
+			log.Fatal(error)
+		}
+	}
 }
